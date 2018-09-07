0511bcfebd2707b34f5041b3320d6fcc5059cd2a
chengbin <sbkim0407@gmail.com>
2018-09-07 17:52:04 +0900

    add main.py

---
+++ src/main.py
@@ -0,0 +1,39 @@
+# ---------------------------------------------------------
+# Tensorflow DiscoGAN Implementation
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Cheng-Bin Jin
+# Email: sbkim0407@gmail.com
+# ---------------------------------------------------------
+import os
+import tensorflow as tf
+from solver import Solver
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_string('gpu_index', '0', 'gpu index if you have multiple gpus, default: 0')
+tf.flags.DEFINE_integer('batch_sizse', 256, 'batch size, default: 256')
+tf.flags.DEFINE_string('dataset', 'bag2shoes', 'dataset name, default bag2shoes')
+tf.flags.DEFINE_bool('is_train', True, 'training or inference mode, default: True')
+
+tf.flags.DEFINE_float('learning_rate', 2e-4, 'initial learning rate for Adam, default: 0.0002')
+tf.flags.DEFINE_float('beta1', 0.5, 'momentum term of Adam, default: 0.5')
+tf.flags.DEFINE_integer('iters', 200000, 'number of iterations, default: 200000')
+tf.flags.DEFINE_integer('print_freq', 100, 'print frequency for loss, default: 100')
+tf.flags.DEFINE_integer('save_freq', 1000, 'save frequency for model, default: 1000')
+tf.flags.DEFINE_integer('sample_freq', 200, 'sample frequency for saving image, default: 200')
+tf.flags.DEFINE_string('load_model', None, 'folder of saved model taht you wish to continue training '
+                       '(e.g. 20180907-1739), default: None')
+
+
+def main(_):
+    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu_index
+
+    solver = Solver(FLAGS)
+    if FLAGS.is_train:
+        solver.train()
+    if not FLAGS.is_train:
+        solver.test()
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ src/solver.py
@@ -0,0 +1,16 @@
+# Tensorflow DiscoGAN Implementation
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Cheng-Bin Jin
+# Email: sbkim0407@gmail.com
+# ---------------------------------------------------------
+
+
+class Solver(object):
+    def __init__(self, flags):
+        print('Hello solver initializer!')
+
+    def train(self):
+        print('Hello solver train function!')
+
+    def test(self):
+        print('Hello solver test function!')
